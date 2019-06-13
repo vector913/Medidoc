0bf4331b2009898642ca9b228e7fc61728395355
JaesukPark <vector913@naver.com>
2019-06-13 22:17:32 +0900

    join.java update

--- app/src/main/java/com/example/medidoc/join.java
+++ app/src/main/java/com/example/medidoc/join.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -19,10 +20,10 @@ public class join extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join_layout);
 
-        SharedPreferences settings = getApplicationContext().getSharedPreferences("mediSettings",0);
-        String mediSettings = settings.getString("mediSettings",null);
-        SharedPreferences.Editor editor = settings.edit();
+        final SharedPreferences settings = getApplicationContext().getSharedPreferences("mediSettings",0);
+       final String mediSettings = settings.getString("mediSettings",null);
 
+        final RadioGroup rg = findViewById(R.id.join_rg);
         final EditText id = findViewById(R.id.join_id);
         final EditText pswd = findViewById(R.id.join_password);
         final EditText pswd_ch = findViewById(R.id.join_password_check);
@@ -38,34 +39,61 @@ public class join extends AppCompatActivity {
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int check = 0;
                 String ids = id.getText().toString();
                 if(ids == null){
                     Toast.makeText(getApplicationContext(),"ID를 입력해주세요!", Toast.LENGTH_LONG);
+                    check = 1;
                 }
                 String pswds = pswd.getText().toString();
                 if(pswds == null){
                     Toast.makeText(getApplicationContext(),"패스워드를 입력해주세요!", Toast.LENGTH_LONG);
+                    check = 1;
                 }
                 String pswchs = pswd_ch.getText().toString();
                 if(pswchs == null){
                     Toast.makeText(getApplicationContext(),"ID를 입력해주세요!", Toast.LENGTH_LONG);
+                    check = 1;
                 }
                 String birthdates = birthdate.getText().toString();
                 if( birthdates == null){
                     Toast.makeText(getApplicationContext(),"생년 월일을 입력해주세요!", Toast.LENGTH_LONG);
+                    check = 1;
                 }
-                String weights = weight.getText().toString();
-                if(weights == null){
+                int sexs = rg.getCheckedRadioButtonId();
+                if(sexs == R.id.join_boy){
+                    sexs = 0;
+                }else if(sexs == R.id.join_girl){
+                    sexs = 1;
+                }else{
+                    Toast.makeText(getApplicationContext(),"성별을 선택바랍니다.",Toast.LENGTH_LONG);
+                    check = 1;
+                }
+
+                int weights;
+                String tmp = weight.getText().toString();
+                weights = Integer.parseInt(tmp);
+                if(weights == 0){
                     Toast.makeText(getApplicationContext(),"몸무게를 입력해주세요!", Toast.LENGTH_LONG);
+                    check = 1;
                 }
                 String specifis = spec.getText().toString();
                 if(specifis == null){
                     Toast.makeText(getApplicationContext(),"특이사항을 입력해주세요!", Toast.LENGTH_LONG);
+                    check = 1;
                 }
                 String emails = email.getText().toString();
                 if(emails == null){
                     Toast.makeText(getApplicationContext(),"email을 입력해주세요!", Toast.LENGTH_LONG);
+                    check = 1;
                 }
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString("userid",ids);
+                editor.putString("userpswd",pswds);
+                editor.putInt("userweight",weights);
+                editor.putString("userspec",specifis);
+                editor.putString("useremail",emails);
+                editor.putInt("usersex",sexs);
             }
         });
 
